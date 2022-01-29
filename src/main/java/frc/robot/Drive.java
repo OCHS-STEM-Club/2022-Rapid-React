bade0fc78855e22cdcc861551aa48c8a9311c1df
EliDaJones <58961015+EliDaJones@users.noreply.github.com>
2022-01-29 10:40:02 -0600

    1/29 made changes to the smart dashboard for encoder values

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -20,7 +20,7 @@ public class Drive {
 
     private XboxController controller = new XboxController(0);
 
-    private double topSpeed = 0.5; //Percent value for drive motor speed from 0 to 1
+    private double topSpeed = 0.75; //Percent value for drive motor speed from 0 to 1
     private double xStickValue; //Variable to store the value from the Xbox Controller
     private double yStickValue; //Variable to store the value from the Xbox Controller
 
@@ -40,7 +40,7 @@ public class Drive {
      * Main method of driving for Teleop
      */
     public void drive(){
-        topSpeed = SmartDashboard.getNumber("Top Speed", 0.5);
+        topSpeed = SmartDashboard.getNumber("Top Speed", 0.75);
         xStickValue = controller.getRawAxis(4) * topSpeed;
         yStickValue = -controller.getRawAxis(1) * topSpeed;
         differentialDrive.arcadeDrive(xStickValue, yStickValue, false);
@@ -50,8 +50,16 @@ public class Drive {
     public double driveMotorPosition(){
         double driveMotorLeftPosition = driveMotorLeft1.getSelectedSensorPosition(3);
         double driveMotorRightPosition = driveMotorRight1.getSelectedSensorPosition(5);
+        
+        double driveMotorLeftPosition2 = driveMotorLeft2.getSelectedSensorPosition(4);
+        double driveMotorRightPosition2 = driveMotorRight2.getSelectedSensorPosition(6);
+
         SmartDashboard.putNumber("Left motor Encoder", driveMotorLeftPosition);
+        SmartDashboard.putNumber("Left motor 2 Encoder", driveMotorLeftPosition2);
+
         SmartDashboard.putNumber("Right motor Encoder", driveMotorRightPosition);
+        SmartDashboard.putNumber("Right motor 2 Encoder", driveMotorRightPosition2);
+
         return driveMotorRightPosition;
         
     }
